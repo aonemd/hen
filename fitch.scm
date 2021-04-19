3c604a10ec9eafddfb35b8f80301c3c0d195ee74
aonemd <aonemdsaleh@gmail.com>
2021-04-19 23:13:13 +0200

    Add current $USER to the output

--- fitch.scm
+++ fitch.scm
@@ -1,7 +1,16 @@
 (import chicken.string)
 (import shell)
 
-(define (host)
-  (conc "Host: " (capture "hostname")))
+(define (main)
+  (display (conc (user) "@" (host))))
 
-(display (host))
+(define (user)
+  (_string-strip (capture "echo $USER")))
+
+(define (host)
+  (_string-strip (capture "hostname")))
+
+(define (_string-strip str)
+  (string-chomp str "\n"))
+
+(main)
